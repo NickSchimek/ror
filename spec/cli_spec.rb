ef07963bd7308b0e3c87806f15f5a4088c8bb9b4
Nick Schimek <cfme00@gmail.com>
2018-08-17 18:15:13 -0700

    Add validation spec for method parameter
    Update test
    
    The supported methods is now being called twice. Once to validate and
    then another time when a method is being called without a class.
    
    This may be able to be refactored so that it is only called once.

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -4,9 +4,19 @@ require 'ror/cli'
 RSpec.describe Ror::CLI do
 
   it 'can display info when the method belongs to one class' do
-    expect(Ror::SupportedMethods).to receive(:render).and_return([:actionview])
+    expect(Ror::SupportedMethods).to receive(:render).and_return([:actionview]).twice
     expect(IO).to receive(:popen).and_return('Method: render\n Class: Actionview')
     result = Ror::CLI.new.info 'render'
     expect(result).to eq('Method: render\n Class: Actionview')
+  end
+
+  it 'validates method argument' do
+    allow(Ror::SupportedMethods).to receive(:render).and_raise("NoMethodError").once
+    expect { Ror::CLI.new.info 'render' }.to output("Sorry, method not found. Please add it and submit a PR\n").to_stdout
+  end
+
+  it 'does not evaluate class with an invalid method' do
+    allow(Ror::SupportedMethods).to receive(:render).and_raise("NoMethodError").once
+    expect { Ror::CLI.new.info 'render', 'actionview' }.to output("Sorry, method not found. Please add it and submit a PR\n").to_stdout
   end
 end
