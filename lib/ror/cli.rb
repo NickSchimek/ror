95dfdf10ecd09336df889c608cced828f55e35e6
Nick Schimek <cfme00@gmail.com>
2018-08-18 23:09:35 -0700

    Initialize modus as an instance variable

--- lib/ror/cli.rb
+++ lib/ror/cli.rb
@@ -7,7 +7,7 @@ module Ror
 
     desc "info METHOD CLASS", "Display info for the desired method"
     def info modus, klass = nil
-      Ror::Info.new.display(modus, klass)
+      Ror::Info.new(modus).display(klass)
     end
 
     desc "new_method", "Generates a scaffold for adding new methods"
--- lib/ror/info.rb
+++ lib/ror/info.rb
@@ -1,55 +1,60 @@
 module Ror
   class Info
 
-    def display modus, klass
-      method_validated = klasses = validate? modus
+    def initialize modus
+      @modus = modus
+      #@klass = klass
+    end
+
+    def display klass
+      method_validated = klasses = validate?
       if system_can_retrieve_class? method_validated, klass
-        klass = get_klass modus, klasses
+        klass = get_klass klasses
         klass_validated = true
       end
       klass_validated = valid_klass?(klass, klasses) if validation_passed? method_validated, !klass_validated
       if validation_passed? method_validated, klass_validated
-        return display_info(modus, klass)
+        return display_info klass
       elsif !method_validated
         return method_does_not_exist_error
       else
-        display_class_error modus
+        display_class_error
       end
     end
 
     private
-    def display_info modus, klass
-      result = method_class modus, klass
+    def display_info klass
+      result = method_class klass
       IO.popen("less", "w") { |f| f.puts IO.read(result) } if result
     end
 
-    def method_class modus, klass
-      result = ror_class klass, modus
-      result.send :show, modus if result
+    def method_class klass
+      result = ror_class klass
+      result.send :show, @modus if result
     end
 
-    def ror_class klass, modus
+    def ror_class klass
       case klass
       when 'actionview', 'view', 'v'
         Ror::Actionview
       when 'actioncontroller', 'controller', 'c'
         Ror::Actioncontroller
       else
-        display_class_error modus
+        display_class_error
       end
     end
 
-    def display_class_error modus
-      puts "Undefined class option: Use 'ror info #{modus}' to view class options."
+    def display_class_error
+      puts "Undefined class option: Use 'ror info #{@modus}' to view class options."
     end
 
     def system_can_retrieve_class? method_validated, klass
       method_validated and !klass
     end
 
-    def validate? modus
+    def validate?
       begin
-        Ror::SupportedMethods.send modus
+        Ror::SupportedMethods.send @modus
       rescue
         false
       end
@@ -76,21 +81,21 @@ module Ror
       puts "Sorry, method not found. Feel free to add it to expand the knowledge store!"
     end
 
-    def get_klass modus, klasses
-      method_belongs_to_one_class?(klasses) ? klasses.first.to_s : ask_user(klasses, modus)
+    def get_klass klasses
+      method_belongs_to_one_class?(klasses) ? klasses.first.to_s : ask_user(klasses)
     end
 
     def method_belongs_to_one_class? klasses
       klasses.length == 1
     end
 
-    def ask_user klasses, modus
-      puts display_message(modus), display_klasses(klasses), ask_for_input(modus)
+    def ask_user klasses
+      puts display_message, display_klasses(klasses), ask_for_input
       retrieve_selection
     end
 
-    def display_message modus
-      "\nMultiple classes contain the #{modus} method.\nFor:"
+    def display_message
+      "\nMultiple classes contain the #{@modus} method.\nFor:"
     end
 
     def display_klasses klasses
@@ -107,8 +112,8 @@ module Ror
       "Actioncontroller type 'controller' or 'c'"
     end
 
-    def ask_for_input modus
-      "\nPlease choose a class for the #{modus} method? "
+    def ask_for_input
+      "\nPlease choose a class for the #{@modus} method? "
     end
 
     def retrieve_selection
